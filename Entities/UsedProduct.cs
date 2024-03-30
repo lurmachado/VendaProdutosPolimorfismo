d4d740f118adfa56e1f49be17cb0e1cfbf33e85f
Luciano Machado <lurmachado@hotmail.com>
2024-03-29 21:11:40 -0300

    Vendas de Produtos usando Polimorfismo

---
+++ Entities/UsedProduct.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Globalization;
+
+namespace VendaProdutos.Entities
+{
+    class UsedProduct: Product
+    {
+        public DateTime ManufactureDate { get; set; }
+
+        public UsedProduct()
+        {
+        }
+
+        public UsedProduct(string name, double price, DateTime manufactureDate)
+            : base(name, price)
+        {
+            ManufactureDate = manufactureDate;
+        }
+
+        public override string PriceTag()
+        {
+            return Name
+                + " (used) $ "
+                + Price.ToString("F2", CultureInfo.InvariantCulture)
+                + " (Manufacture date: "
+                + ManufactureDate.ToString("dd/MM/yyyy")
+                + ")";
+        }
+    }
+}
+
